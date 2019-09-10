6d8631799888d91329ebbe68ae15067f8550d717
foise <live.fistful@gmail.com>
2019-09-10 17:25:19 +0300

    added button search

--- labweb1/js/searchByText.js
+++ labweb1/js/searchByText.js
@@ -1,13 +1,9 @@
-document.querySelector('#card') = function () {
-  let valSearch = this.value.trim();
+document.querySelector('#buttonSearch').onclick = function () {
+  let valSearch = card.value.trim();
   let cardItems = document.querySelectorAll('.card');
   cardItems.forEach(function (elem) {
-    if (valSearch !== '') {
-      if (elem.innerText.search(valSearch) === -1) {
-        elem.classList.add('hide');
-      } else {
-        elem.classList.remove('hide');
-      }
+    if (valSearch !== '' && elem.innerText.search(valSearch) === -1) {
+      elem.classList.add('hide');
     } else {
       elem.classList.remove('hide');
     }
