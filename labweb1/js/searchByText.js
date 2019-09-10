215aa7ce9482e63189c8d714c5753bdff590e99c
foise <live.fistful@gmail.com>
2019-09-10 17:53:38 +0300

    fixed js

--- labweb1/js/searchByText.js
+++ labweb1/js/searchByText.js
@@ -1,6 +1,6 @@
 document.querySelector('#buttonSearch').onclick = function () {
-  let valSearch = card.value.trim();
-  let cardItems = document.querySelectorAll('.card');
+  let valSearch = document.getElementById('card-search').value.trim();
+  let cardItems = document.querySelectorAll('.column');
   cardItems.forEach(function (elem) {
     if (valSearch !== '' && elem.innerText.search(valSearch) === -1) {
       elem.classList.add('hide');
