d92b091d2fce5092acf7fac92730f1e492d7ca43
foise <live.fistful@gmail.com>
2019-10-22 17:02:48 +0300

    fixed issues

--- labweb4/totalCommanderForWeb/js/switch.js
+++ labweb4/totalCommanderForWeb/js/switch.js
@@ -1,33 +1,30 @@
 window.addEventListener('keydown', function (event) {
   var content = document.querySelectorAll('.content');
   var directories = document.querySelectorAll('.directory-contents');
-  var secondDirContents = directories[1].querySelectorAll('.content');
-  var firstDirContents = directories[0].querySelectorAll('.content');
+  var secondDirContents = directories[1].getElementsByClassName('content');
+  var firstDirContents = directories[0].getElementsByClassName('content');
+
+  function ChooseContent(fromDir, inDir) {
+    for (let element of fromDir) {
+      if (element.classList.contains('chosen')) {
+        element.classList.toggle('chosen');
+        inDir.item(0).classList.toggle('chosen');
+      }
+    }
+  }
 
   //ArrowRight
   if (event.keyCode === 39) {
-    firstDirContents.forEach(function (element, index) {
-      if (element.classList.contains('chosen')) {
-        element.classList.toggle('chosen');
-        secondDirContents[0].classList.toggle('chosen');
-      }
-    });
+    ChooseContent(firstDirContents, secondDirContents);
   }
 
   //ArrowLeft
   if (event.keyCode === 37) {
-    secondDirContents.forEach(function (element, index) {
-      if (element.classList.contains('chosen')) {
-        element.classList.toggle('chosen');
-        firstDirContents[0].classList.toggle('chosen');
-      }
-    });
-
+    ChooseContent(secondDirContents, firstDirContents);
   }
 
   //ArrowDown
   if (event.keyCode === 40) {
-    var bool = false;
     content.forEach(function (element, index) {
       if (element.classList.contains('chosen') && !bool && index + 1 < content.length) {
         element.classList.toggle('chosen');
