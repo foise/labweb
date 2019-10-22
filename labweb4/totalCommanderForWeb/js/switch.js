2bcfe29a60639c27d91f78caf1a08016b7151322
foise <live.fistful@gmail.com>
2019-10-22 17:23:27 +0300

    fixed js

--- labweb4/totalCommanderForWeb/js/switch.js
+++ labweb4/totalCommanderForWeb/js/switch.js
@@ -4,45 +4,49 @@ window.addEventListener('keydown', function (event) {
   var secondDirContents = directories[1].getElementsByClassName('content');
   var firstDirContents = directories[0].getElementsByClassName('content');
 
-  function ChooseContent(fromDir, inDir) {
-    for (let element of fromDir) {
-      if (element.classList.contains('chosen')) {
-        element.classList.toggle('chosen');
-        inDir.item(0).classList.toggle('chosen');
-      }
+  function ChooseContent(selectionType, fromDir, inDir, direction) {
+    switch (selectionType) {
+      case 1:
+        for (let element of fromDir) {
+          if (element.classList.contains('chosen')) {
+            element.classList.toggle('chosen');
+            inDir.item(0).classList.toggle('chosen');
+          }
+        }
+        break;
+      case 2:
+        var bool = false;
+        content.forEach(function (element, index) {
+          condition = direction === "down" ? index + 1 < content.length : index - 1 >= 0;
+          operator = direction === "down" ? index + 1 : index - 1;
+          if (element.classList.contains('chosen') && !bool && condition) {
+            element.classList.toggle('chosen');
+            bool = true;
+            content[operator].classList.toggle('chosen');
+          }
+        });
+        break;
     }
+
   }
 
   //ArrowRight
   if (event.keyCode === 39) {
-    ChooseContent(firstDirContents, secondDirContents);
+    ChooseContent(1, firstDirContents, secondDirContents);
   }
 
   //ArrowLeft
   if (event.keyCode === 37) {
-    ChooseContent(secondDirContents, firstDirContents);
+    ChooseContent(1, secondDirContents, firstDirContents);
   }
 
   //ArrowDown
   if (event.keyCode === 40) {
-    content.forEach(function (element, index) {
-      if (element.classList.contains('chosen') && !bool && index + 1 < content.length) {
-        element.classList.toggle('chosen');
-        bool = true;
-        content[index + 1].classList.toggle('chosen');
-      }
-    });
+    ChooseContent(2, secondDirContents, firstDirContents, "down");
   }
 
   //ArrowUp
   if (event.keyCode === 38) {
-    var bool = false;
-    content.forEach(function (element, index) {
-      if (element.classList.contains('chosen') && !bool && index - 1 >= 0) {
-        element.classList.toggle('chosen');
-        bool = true;
-        content[index - 1].classList.toggle('chosen');
-      }
-    });
+    ChooseContent(2, secondDirContents, firstDirContents, "up");
   }
 }, true);
